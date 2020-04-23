a9254f17f68b9d0c657ea6e11bbf282220b1e995
Smridhi Patwari <patwarismridhi@gmail.com>
2020-04-23 17:09:02 +0800

    Update RPi _version_colour_recognition.py

--- RPi _version_colour_recognition.py
+++ RPi _version_colour_recognition.py
@@ -4,7 +4,9 @@ import numpy as np
 import math
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import 
+from std_msgs.msg import String
+
 
 # setting the raspberry pi resolution and frame rate
 camera = PiCamera()
@@ -17,6 +19,7 @@ width = rawCapture.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = rawCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
 #centre of image
 centre = (int(width/2), int(height/2))
+
 # allow the camera to warmup
 time.sleep(0.1)
 
@@ -24,6 +27,13 @@ time.sleep(0.1)
 rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 rot = Twist()
 
+
+#to publish the percentage of the red colour detected 
+target_pub = rospy.Publisher("auto_nav/seen_target", String, queue_size=1)
+percentage_1 = float(0)
+
+# to get the "Done" message from auto_nav to continue target detection
+nav_done = rospy.Subscriber("auto_nav/navdone", String, queue_size=1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	image = frame.array
@@ -47,41 +57,46 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	tot_pixel = hsv_frame.size
 	colour_pixel = np.count_nonzero(red_image)
 	percentage = round(colour_pixel * 100 / tot_pixel, 2)
-    
-	if percentage != float(0):
-		print('percentage is', percentage)
+	
+	#for auto_nav to save the coordinates of the position where highest percentage of red is detected
+    	if percentage > percentage_1 and percentage >= 4 :
+		percentage_1 = percentage
+		target_pub.publish(String("colour detected"))
+	#print('percentage is', percentage)
+		
 	'''if detected percentage is !=0, and percentage <4 move bot towards the target
 	if percentage is at least 4 %, shoot'''
 
 	gray = cv2.cvtColor(red_image, cv2.COLOR_BGR2GRAY)
 	ret, threshImg = cv2.threshold(gray, 27, 255, cv2.THRESH_BINARY)
 	drawnImage = red_image
-    # find the contours in the thresholded image...
-	contours, high = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	
-	for c in contours:
-		if cv2.contourArea(c) < 4000:
-			continue
-		(x,y,w,h)= cv2.boundingRect(c)
-		cv2.rectangle(drawnImage,(x,y),(x+w,y+h),(255,0,0),2)
+	if nav_done == "Done":
+  	  # find the contours in the thresholded image...
+		contours, high = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+		for c in contours:
+			if cv2.contourArea(c) < 4000:
+				continue
+			(x,y,w,h)= cv2.boundingRect(c)
+			cv2.rectangle(drawnImage,(x,y),(x+w,y+h),(255,0,0),2)
 
-		Centroid = (int((x + x + w) /2), int((y + y + h) /2))
-		cv2.circle(drawnImage, Centroid, 1, (54, 255, 164), 4)
-		cv2.circle(drawnImage, centre, 1, (115, 0, 255), 4)
-		
-		dist_from_cent = [centre[0]-Centroid[0], centre[1]-Centroid[1]]
-		print ('centre_dist =', dist_from_cent)
+			Centroid = (int((x + x + w) /2), int((y + y + h) /2))
+			cv2.circle(drawnImage, Centroid, 1, (54, 255, 164), 4)
+			cv2.circle(drawnImage, centre, 1, (115, 0, 255), 4)
 
-		'''if dist_from_centre[0] <0:
-			turn bot towards right
-		elif dist_from_centre[0]>20:
-			turn bot left 
-		elif dist_from_centre[1] <0:
-			move tilt up 
-		elif dist_from_centre[1] >0:
-			move tilt down
-		else:
-			shoot '''
+			dist_from_cent = [centre[0]-Centroid[0], centre[1]-Centroid[1]]
+			print ('centre_dist =', dist_from_cent)
+
+			'''if dist_from_centre[0] <0:
+				turn bot towards right
+			elif dist_from_centre[0]>20:
+				turn bot left 
+			elif dist_from_centre[1] <0:
+				move tilt up 
+			elif dist_from_centre[1] >0:
+				move tilt down
+			else:
+				shoot '''
 
 	cv2.imshow(winName, drawnImage)
 
